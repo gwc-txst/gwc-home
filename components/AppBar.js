22e1ca7775d0bba2129fd14a5db3514847025b0d
Victoria Jordan <vjordan.cs@gmail.com>
2022-07-07 22:12:37 -0500

    feat: build link page

--- components/AppBar.js
+++ components/AppBar.js
@@ -3,17 +3,24 @@ import Image from "next/image";
 const AppBar = () => {
   return (
     <div>
-      <nav className="h-24 bg-red-200 grid grid-cols-12">
-        <div className="bg-blue-400 col-span-2 flex justify-center no-wrap">
-          <div className="w-full flex items-center ml-4 "></div>
+      <nav className="h-32 bg-gradient-to-r from-[#B6FFDD] via-[#788DFF] to-[#FF94FF] grid grid-cols-12">
+        <div className="col-span-2 flex justify-center no-wrap border">
+          <div className="w-full flex items-center ml-6">
+            {/* <Image
+              src="/../public/logo.png"
+              width="130px"
+              height="87px"
+              layout="fixed"
+            /> */}
+          </div>
         </div>
-        <div className="col-span-6 bg-blue-400 col-start-4 flex justify-center no-wrap">
+        <div className="col-span-6 border col-start-4 flex justify-center no-wrap">
           <div className="w-full flex justify-center items-center">page1</div>
           <div className="w-full flex justify-center items-center">page2</div>
           <div className="w-full flex justify-center items-center">page3</div>
           <div className="w-full flex justify-center items-center">page4</div>
         </div>
-        <div className="col-span-2 col-start-11 bg-blue-400 flex justify-center no-wrap">
+        <div className="col-span-2 col-start-11 border flex justify-center no-wrap">
           <div className="w-full flex justify-end items-center mr-4">other</div>
         </div>
       </nav>
--- pages/our-links.js
+++ pages/our-links.js
@@ -1,9 +1,17 @@
 import AppBar from "../components/AppBar";
+import CuteButton from "../components/CuteButton";
 
 export default function OurLinks() {
   return (
-    <div className="min-h-screen">
+    <div>
       <AppBar />
+      <div className="bg-[#242424] h-screen flex flex-col justify-center items-center gap-6">
+        <CuteButton label="Shawna" />
+        <CuteButton label="Stephen" />
+        <CuteButton label="Victoria" />
+        <CuteButton label="everyone" />
+        <CuteButton label="there" />
+      </div>
     </div>
   );
 }
