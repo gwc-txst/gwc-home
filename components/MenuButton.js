dd13027705c864e888d37d78752124740912e962
Bridget Bangert <bmb191@txstate.edu>
2022-07-21 11:22:33 -0500

    feat: add component for menu buttons

---
+++ components/MenuButton.js
@@ -0,0 +1,7 @@
+const MenuButton = ({ label }) => {
+  return (
+    <div className="cursor-pointer hover:text-blue-200 text-white">{label}</div>
+  );
+};
+
+export default MenuButton;
--- pages/links.js
+++ pages/links.js
@@ -1,21 +1,22 @@
 import LinktreeButton from "../components/LinktreeButton";
+import MenuButton from "../components/MenuButton";
 
 export default function Links() {
   return (
-    <div className="h-cover bg-gradient-to-tr from-[#B6FFDD] via-[#788DFF] to-[#FF94FF]">
-      {/* Start header */}
+    <div className="h-screen bg-gradient-to-tr from-[#B6FFDD] via-[#788DFF] to-[#FF94FF]">
+      {/* Start menu */}
       <div className="bg-black h-24 grid grid-cols-3 items-center">
         <div className="text-white col-span-1 mx-6 font-babe-sans sm:text-2xl text-lg cursor-pointer">
           Girls Who Code TXST
         </div>
-        {/* We could probably make these buttons their own class for cleaner code */}
-        <div className="flex space-x-6 col-span-2 justify-end mx-6 text-white">
-          <div className="cursor-pointer hover:text-blue-200">Home</div>
-          <div className="cursor-pointer hover:text-blue-200">Donate</div>
-          <div className="cursor-pointer hover:text-blue-200">About</div>
-          <div className="cursor-pointer hover:text-blue-200">Links</div>
+        <div className="flex space-x-6 col-span-2 justify-end mx-6">
+          <MenuButton label="Home" />
+          <MenuButton label="Donate" />
+          <MenuButton label="About" />
+          <MenuButton label="Links" />
         </div>
       </div>
+
       {/*  Start links */}
       <div className="text-center text-2xl mt-12 cursor-default">
         Here are our links!
@@ -29,7 +30,6 @@ export default function Links() {
         <LinktreeButton />
         <LinktreeButton />
       </div>
-      {/* Having trouble putting space under the last button */}
     </div>
   );
 }
